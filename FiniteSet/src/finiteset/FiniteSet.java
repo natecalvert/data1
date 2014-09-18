36069f43c3c1177ba2d8c92a8c0f0c05dbc21b1b
natecalvert <natecalvert@Stormageddon.local>
2014-09-18 09:28:29 -0400

    Some minor stuff
    -Cleaned up formatting a bit.
    -My understanding of how a call to subset works got reversed for
    testing. Fixed. It seems to be working now, as does equals.
    -Reduced max possible length for randomBSTs to increase runtime, and
    prevent stalling to find a unique int to add. [idea via tomlum comment,
    thanks.]

--- FiniteSet/src/finiteset/FiniteSet.java
+++ FiniteSet/src/finiteset/FiniteSet.java
@@ -4,102 +4,114 @@ import java.util.*;
 
 public class FiniteSet {
 
-    interface BST{
+    interface BST {
+
         public int cardinality();
+
         public boolean isEmptyHuh();
-        public boolean member( int elt);
-        public BST add( int elt);
-        public BST remove( int elt);
-        public BST union( BST tree);
-        public BST inter( BST tree);
-        public BST diff( BST tree);
-        public boolean equal( BST tree);
-        public boolean subset( BST tree);
+
+        public boolean member(int elt);
+
+        public BST add(int elt);
+
+        public BST remove(int elt);
+
+        public BST union(BST tree);
+
+        public BST inter(BST tree);
+
+        public BST diff(BST tree);
+
+        public boolean equal(BST tree);
+
+        public boolean subset(BST tree);
     }
-    
+
     static class Empty implements BST {
-        
-        Empty(){ }
-        
-        public int cardinality(){
+
+        Empty() {
+        }
+
+        public int cardinality() {
             return 0;
         }
-        
-        public boolean isEmptyHuh(){
+
+        public boolean isEmptyHuh() {
             return true;
         }
-        
-        public boolean member( int elt){
+
+        public boolean member(int elt) {
             return false;
         }
-        
-        public BST add( int elt){
-            return new Tree( elt, new Empty(), new Empty());
+
+        public BST add(int elt) {
+            return new Tree(elt, new Empty(), new Empty());
         }
-        
-        public BST remove( int elt){
+
+        public BST remove(int elt) {
             return new Empty();
         }
-        
-        public BST union( BST tree){
+
+        public BST union(BST tree) {
             return tree;
         }
-        
-        public BST inter( BST tree){
+
+        public BST inter(BST tree) {
             return new Empty();
         }
-        
-        public BST diff( BST tree){
+
+        public BST diff(BST tree) {
             return tree;
-        }       
-        
-        public boolean equal( BST tree){
+        }
+
+        public boolean equal(BST tree) {
             return tree.isEmptyHuh();
         }
-        
-        public boolean subset( BST tree){
+
+        public boolean subset(BST tree) {
             return true;
         }
     }
-    
+
     static class Tree implements BST {
+
         int data;
         BST left;
         BST right;
-        
-        Tree (int data, BST left, BST right) {
+
+        Tree(int data, BST left, BST right) {
             this.data = data;
             this.left = left;
             this.right = right;
         }
-        
-        public BST empty(){
+
+        public BST empty() {
             return new Empty();
         }
-        
-        public int cardinality(){
+
+        public int cardinality() {
             return 1 + left.cardinality() + right.cardinality();
         }
-        
-        public boolean isEmptyHuh(){
+
+        public boolean isEmptyHuh() {
             return false;
         }
-        
-        public boolean member( int elt){
+
+        public boolean member(int elt) {
             return (data == elt || left.member(elt) || right.member(elt));
         }
-             
-        public BST add( int elt){
+
+        public BST add(int elt) {
             if (data == elt) {
                 return this;
-            } else if (elt < data){
+            } else if (elt < data) {
                 return new Tree(data, left.add(elt), right);
             } else {
                 return new Tree(data, left, right.add(elt));
             }
         }
-             
-        public BST remove( int elt){
+
+        public BST remove(int elt) {
             if (elt == data) {
                 return left.union(right);
             } else if (elt < data) {
@@ -108,90 +120,85 @@ public class FiniteSet {
                 return new Tree(data, left, right.remove(elt));
             }
         }
-        
-        public BST union( BST tree){
+
+        public BST union(BST tree) {
             return this.left.union(tree.union(this.right).add(this.data));
         }
-        
-        public BST inter( BST tree){
-            if (tree.member(data)){
+
+        public BST inter(BST tree) {
+            if (tree.member(data)) {
                 return new Tree(data, left.inter(tree), right.inter(tree));
             } else {
                 return left.inter(tree).union(right.inter(tree));
             }
         }
 
-// diff should return what is in this and not in tree, currently returns
+// diff should return what is in this and not in tree? currently returns
 // what is in this and not in tree as well as what is in tree and not in this
-        
-        public BST diff( BST tree){
+        public BST diff(BST tree) {
             if (tree.member(data)) {
                 return left.diff(tree).union(right.diff(tree));
             } else {
                 return new Tree(data, left.diff(tree), right.diff(tree));
             }
         }
-        
-        public boolean equal( BST tree){
+
+        public boolean equal(BST tree) {
             return (this.subset(tree) && tree.subset(this));
         }
-        
-        public boolean subset( BST tree){
+
+        public boolean subset(BST tree) {
             return (tree.member(data) && left.subset(tree) && right.subset(tree));
         }
     }
-    
+
 // Random BST stuff
-        
-        static Random randy = new Random();
-        public static int randomInt(int min, int max) {
-            return randy.nextInt((max-min)+1)+min;
-        }
-        
-        public static BST randomBST(int length){
-            BST temp;
-            if (length == 0) {
-                return temp;
+    static Random randy = new Random();
+
+    public static int randomInt(int min, int max) {
+        return randy.nextInt((max - min) + 1) + min;
+    }
+
+    public static BST randomBST(int length) {
+        BST temp;                            // temp not initialized, could make helper function?
+        if (length == 0) {
+            return temp;
+        } else {
+            int randyInt = randomInt(0, 100);
+            if (!(temp.member(randyInt))) {
+                temp.add(randyInt);
+                return randomBST(length - 1);
             } else {
-                int randyInt = randomInt(0, 100);
-                if (!(temp.member(randyInt))){
-                    temp.add(randyInt);
-                    return randomBST(length-1);
-                } else{
-                    return randomBST(length);
-                }
+                return randomBST(length);
             }
         }
-            
+    }
+
     public static void main(String[] args) {
-        
-// CHECK BST PROPERTY IS HELD THROUGHOUT
-        //I think it is?
-        
-        System.out.println("Gonna do some testing below:");
+
+        System.out.println("Gonna do some testing below!");
         System.out.println("");
 
 // Very basic testing, checking that things seem to be okay. NOT EXHAUSTIVE. No fun properties, yet.
-
         BST MT = new Empty();
-        BST t_4 = new Tree( 4, MT, MT );
-        BST t_5 = new Tree( 5, MT, MT );
-        BST t_7 = new Tree( 7, MT, MT );
-        BST t_8 = new Tree( 8, MT, MT );
-        BST t_6 = new Tree( 6, t_5, t_7);
-       
+        BST t_4 = new Tree(4, MT, MT);
+        BST t_5 = new Tree(5, MT, MT);
+        BST t_7 = new Tree(7, MT, MT);
+        BST t_8 = new Tree(8, MT, MT);
+        BST t_6 = new Tree(6, t_5, t_7);
+
         System.out.println("Cardinality Tests:");
         System.out.println(MT.cardinality() + " should be " + 0);
         System.out.println(t_5.cardinality() + " should be " + 1);
         System.out.println(t_6.cardinality() + " should be " + 3);
         System.out.println("");
-        
+
         System.out.println("isEmptyHuh Tests:");
         System.out.println(MT.isEmptyHuh() + " should be " + true);
         System.out.println(t_5.isEmptyHuh() + " should be " + false);
         System.out.println(t_6.isEmptyHuh() + " should be " + false);
         System.out.println("");
-        
+
         System.out.println("member Tests:");
         System.out.println(MT.member(5) + " should be " + false);
         System.out.println(t_5.member(5) + " should be " + true);
@@ -200,7 +207,7 @@ public class FiniteSet {
         System.out.println(t_6.member(6) + " should be " + true);
         System.out.println(t_6.member(9) + " should be " + false);
         System.out.println("");
-        
+
         System.out.println("add Tests:");
         System.out.println(MT.add(5).cardinality() + " should be " + 1);
         System.out.println(t_5.add(4).cardinality() + " should be " + 2);
@@ -208,7 +215,7 @@ public class FiniteSet {
         System.out.println(t_6.add(4).cardinality() + " should be " + 4);
         System.out.println(t_6.add(8).cardinality() + " should be " + 4);
         System.out.println("");
-        
+
         System.out.println("remove Tests:");
         System.out.println(MT.remove(5).cardinality() + " should be " + 0);
         System.out.println(t_5.remove(4).cardinality() + " should be " + 1);
@@ -218,7 +225,7 @@ public class FiniteSet {
         System.out.println(t_6.remove(7).cardinality() + " should be " + 2);
         System.out.println(t_6.remove(5).cardinality() + " should be " + 2);
         System.out.println("");
-        
+
         System.out.println("union Tests:");
         System.out.println(MT.union(MT).isEmptyHuh() + " should be " + true);
         System.out.println(MT.union(t_4).isEmptyHuh() + " should be " + false);
@@ -237,7 +244,7 @@ public class FiniteSet {
         System.out.println(t_6.union(t_8).union(t_4).cardinality() + " should be " + 5);
         System.out.println(t_6.union(t_4).union(t_8).cardinality() + " should be " + 5);
         System.out.println("");
-        
+
         System.out.println("inter Tests:");
         System.out.println(MT.inter(MT).cardinality() + " should be " + 0);
         System.out.println(MT.inter(t_5).cardinality() + " should be " + 0);
@@ -251,8 +258,6 @@ public class FiniteSet {
         System.out.println(t_6.inter(t_8).cardinality() + " should be " + 0);
         System.out.println("");
 
-//Due to specs of diff, these don't work. Need to change function? Or incorrectly spec'd?
-        
         System.out.println("diff Tests:");
         System.out.println(MT.diff(MT).cardinality() + " should be " + 0);
         System.out.println(MT.diff(t_5).cardinality() + " should be " + 1);
@@ -266,7 +271,7 @@ public class FiniteSet {
         System.out.println(t_6.diff(t_7).cardinality() + " should be " + 1);
         System.out.println(t_6.diff(t_8).cardinality() + " should be " + 1);
         System.out.println("");
-        
+
         System.out.println("equal Tests:");
         System.out.println(MT.equal(MT) + " should be " + true);
         System.out.println(t_5.equal(t_4) + " should be " + false);
@@ -276,27 +281,26 @@ public class FiniteSet {
         System.out.println(t_6.equal(t_7) + " should be " + false);
         System.out.println(t_6.equal(t_5) + " should be " + false);
         System.out.println("");
-        
+
         System.out.println("subset Tests:");
         System.out.println(MT.subset(MT) + " should be " + true);
         System.out.println(t_5.subset(t_4) + " should be " + false);
         System.out.println(t_5.subset(t_5) + " should be " + true);
-        System.out.println(t_6.subset(t_4) + " should be " + false);
+        System.out.println(t_4.subset(t_6) + " should be " + false);
         System.out.println(t_6.subset(t_6) + " should be " + true);
-        System.out.println(t_6.subset(t_7) + " should be " + true);
-        System.out.println(t_6.subset(t_5) + " should be " + true);
+        System.out.println(t_7.subset(t_6) + " should be " + true);
+        System.out.println(t_5.subset(t_6) + " should be " + true);
         System.out.println("");
-        
+
 // Just Random BST Things
-        
-        int randyInt = randomInt(0,100);
-        
-        int length1 = randomInt(0,100);
-        int length2 = randomInt(0,100);
-        int length3 = randomInt(0,100);
-        int length4 = randomInt(0,100);
-        int length5 = randomInt(0,100);
-        
+        int randyInt = randomInt(0, 100);
+
+        int length1 = randomInt(0, 10);
+        int length2 = randomInt(0, 20);
+        int length3 = randomInt(0, 30);
+        int length4 = randomInt(0, 40);
+        int length5 = randomInt(0, 50);
+
         BST randa = randomBST(length1);
         BST rande = randomBST(length2);
         BST randi = randomBST(length3);
